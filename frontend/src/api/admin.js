68a968e2713b747cdea835dd8d53aa423193e278
choetaehyeong <taehyoung0528@gmail.com>
2024-11-20 16:04:03 +0900

    Refactor: 관리자 페이지 노션에 있는 axios 요청들 전체 추가

--- frontend/src/api/admin.js
+++ frontend/src/api/admin.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 /** axios 기본 url 및 헤더 설정 */
 const config = axios.create({
-  baseURL: 'http://localhost:8081/api/admin/',  // Spring Boot API 경로
+  baseURL: 'http://localhost:8081/api/admin/auth/',  // Spring Boot API 경로
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
 function getUserListByAdmin(token) {
-  return config.get('auth/user-read', {
+  return config.get('user-read', {
     headers: {
       'Authorization': `Bearer ${token}`
     }
@@ -17,7 +17,51 @@ function getUserListByAdmin(token) {
 }
 
 function getHotelManagerListByAdmin(token){
-  return config.get('auth/hotelAdmin-read', {
+  return config.get('hotelAdmin-read', {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+}
+
+function getUserSearch(token, name){
+  return config.get('user-search', {
+    params: {
+      search : name
+    },
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+}
+
+function getHotelAdminSearch(token, name){
+  return config.get('user-search', {
+    params: {
+      search : name
+    },
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+}
+
+function requestActiveStatus(token, userId){
+  return config.post('hotel', {
+    params: {
+      userId : userId
+    },
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+}
+
+function requestReportControl(token, reportId){
+  return config.post('review-report', {
+    params: {
+      reportId : reportId
+    },
     headers: {
       'Authorization': `Bearer ${token}`
     }
@@ -25,7 +69,7 @@ function getHotelManagerListByAdmin(token){
 }
 
 function getReportListByAdmin(token){
-  return config.get('auth/review-read', {
+  return config.get('review-read', {
     headers: {
       'Authorization': `Bearer ${token}`
     }
@@ -33,7 +77,7 @@ function getReportListByAdmin(token){
 }
 
 function getAcountInfo(token) {
-  return config.get('auth/userInfo', {
+  return config.get('userInfo', {
     headers: {
       'Authorization': `Bearer ${token}`
     }
@@ -41,7 +85,7 @@ function getAcountInfo(token) {
 }
 
 function getReportInfo(token) {
-  return config.get('auth/reviewReportInfo', {
+  return config.get('reviewReportInfo', {
     headers: {
       'Authorization': `Bearer ${token}`
     }
@@ -51,6 +95,10 @@ function getReportInfo(token) {
 export {
   getUserListByAdmin,
   getHotelManagerListByAdmin,
+  getUserSearch,
+  getHotelAdminSearch,
+  requestActiveStatus,
+  requestReportControl,
   getReportListByAdmin,
   getAcountInfo,
   getReportInfo,
