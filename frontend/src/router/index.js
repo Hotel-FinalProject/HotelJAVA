7fb53db5835d12b35d9e40cb31c09960854a22be
choetaehyeong <taehyoung0528@gmail.com>
2024-11-20 09:41:15 +0900

    Feat: 라우터 가드 추가
    권한 확인 후 해당하는 권한으로 이동
    - 관리자 계정에서 /admin/hotel 이동 시 /admin/system으로 이동
    - 호텔 관리자계정에서 /admin/system 이동 시 /admin/hotel로 이동
    - 로그인 되어있지 않은 경우 /admin 이동

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,6 +12,9 @@ import AccountSettings from "@/components/UserPages/AccountSettings.vue";
 import HotelAdminPage from '@/components/HotelAdminPage.vue';
 import SearchPage from '@/components/SearchPage.vue';
 import MapPage from '@/components/MapPage.vue';
+import AdminLogin from '@/components/SystemAdminPages/AdminLogin.vue';
+
+import { jwtDecode } from 'jwt-decode';
 
 const routes = [
   {
@@ -59,11 +62,11 @@ const routes = [
   },
   {
     path: '/find-my-id',
-    component: () => import ('@/components/UserPages/FindId.vue'),
+    component: () => import('@/components/UserPages/FindId.vue'),
   },
   {
     path: '/payment',
-    component: () => import ('@/components/PaymentPage.vue'),
+    component: () => import('@/components/PaymentPage.vue'),
     name: 'paymentPage',
   },
   {
@@ -71,12 +74,18 @@ const routes = [
     component: EmailVerificationPage
   },
   {
+    path: '/admin/:any*',
+    component: AdminLogin,
+  },
+  {
     path: '/admin/hotel',
-    component: HotelAdminPage
+    component: HotelAdminPage,
+    meta: { requiresAuth: true, requiredRole: 'ROLE_HOTEL_MANAGER' }
   },
   {
     path: '/admin/system', // 시스템 관리자 페이지
     component: () => import('@/components/SystemAdminPage.vue'),
+    meta: { requiresAuth: true, requiredRole: 'ROLE_ADMIN' }
   },
   {
     path: '/reset-password', // 이메일 인증 페이지 경로
@@ -99,6 +108,54 @@ const router = createRouter({
   scrollBehavior() {
     return { top: 0 };
   }
+});
+
+router.beforeEach((to, from, next) => {
+  const token = sessionStorage.getItem("token");
+
+  let userRole = null;
+  if (token) {
+    try {
+      const decodedToken = jwtDecode(token);
+      userRole = decodedToken.role;
+    } catch (error) {
+      console.error('토큰 디코딩 중 오류 발생: ', error);
+      return next({ path: '/' });
+    }
+  }
+
+  if (to.path === '/admin') {
+    // '/admin' 경로로 접근할 때
+    if (!token) {
+      // 토큰이 없는 경우 메인 페이지로 리다이렉트
+      return next({ path: '/' });
+    } else if (userRole === 'ROLE_ADMIN') {
+      // ROLE_ADMIN인 경우 '/admin/system'으로 리다이렉트
+      return next({ path: '/admin/system' });
+    } else if (userRole === 'ROLE_HOTEL_MANAGER') {
+      // ROLE_HOTEL_MANAGER인 경우 '/admin/hotel'으로 리다이렉트
+      return next({ path: '/admin/hotel' });
+    } else {
+      // 권한이 없는 경우 메인 페이지로 리다이렉트
+      return next({ path: '/' });
+    }
+  }
+
+  if (to.path === '/admin/hotel' && userRole === 'ROLE_ADMIN') {
+    // 어드민 유저가 '/admin/hotel'로 접근할 경우 '/admin/system'으로 리다이렉트
+    return next({ path: '/admin/system' });
+  }
+
+  if (to.meta.requiresAuth) {
+    // 특정 경로에 대해 권한이 필요할 때
+    if (!token || to.meta.requiredRole !== userRole) {
+      // 토큰이 없거나 요구된 권한이 맞지 않으면 관리자 로그인 페이지로 이동
+      return next({ path: '/admin' });
+    }
+  }
+
+  // 그 외 경로로의 접근은 그대로 허용
+  next();
 });
 
 export default router;
