7842112680302d1ffb84221f8f13989b576d4953
vsuminv <sumin@naver.com>
2024-11-22 13:52:30 +0900

    Refactor : 코드 수정

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 /** axios 기본 url 및 헤더 설정 */
 const config = axios.create({
-  baseURL: '/api',  // Spring Boot API 경로
+  //baseURL: process.env.VUE_APP_API_URL+'/api',  // Spring Boot API 경로(로컬)
+  baseURL: 'http://43.200.45.122/api',
+
   headers: {
     'Content-Type': 'application/json'
   }
@@ -10,7 +12,7 @@ const config = axios.create({
 
 /** 회원가입 */
 function signupUser(userData, verificationToken) {
-  return config.post(process.env.VUE_APP_API_URL+'/users/signup', userData, {
+  return config.post('/users/signup', userData, {
     headers: {
       'verificationToken': `Bearer ${verificationToken}`,
     },
@@ -38,7 +40,7 @@ function findIdUser(name) {
 
 /** 이메일 인증 */
 function sendVerificationEmailAPI(email, mode) {
-  return config.post(process.env.VUE_APP_API_URL+'/users/send-verification-email', {
+  return config.post('/users/send-verification-email', {
     email: email,
     mode: mode,
   });
@@ -46,7 +48,7 @@ function sendVerificationEmailAPI(email, mode) {
 
 /** 이메일 인증 확인 */
 function verifyEmailToken(token) {
-  return config.get(process.env.VUE_APP_API_URL+'/users/verify-email', {
+  return config.get('/users/verify-email', {
     params: {
       token: token
     }
